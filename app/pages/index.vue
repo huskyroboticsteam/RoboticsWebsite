23b733a6b18f54bc5787ae7fa27684c2acbfb770
Jordan Tucker <jordan@jortuck.com>
2026-02-28 12:52:42 -0800

    update SAR video embed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -64,19 +64,19 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 					);
 				"
 			>
-				<RoboticsContainer class="md:py-30 space-y-6 py-20">
-					<div class="md:max-w-1/2 xl:max-w-1/3 space-y-6">
+				<RoboticsContainer class="space-y-6 py-20 md:py-30">
+					<div class="space-y-6 md:max-w-1/2 xl:max-w-1/3">
 						<h1
-							class="leading-18 text-secondary lg:leading-30 text-6xl font-bold opacity-0 lg:text-7xl xl:text-8xl"
+							class="text-6xl leading-18 font-bold text-secondary opacity-0 lg:text-7xl lg:leading-30 xl:text-8xl"
 						>
 							Husky<br />Robotics
 						</h1>
-						<p class="border-b-1 border-b-accent text-secondary invisible pb-5 text-lg">
+						<p class="invisible border-b-1 border-b-accent pb-5 text-lg text-secondary">
 							Rovers modeled, engineered, and field‑tested by UW students for real‑world planetary
 							exploration & industry‑grade team robotics.
 						</p>
 						<p class="invisible space-x-3 align-middle">
-							<span class="font-roboto tracking-robotics text-secondary font-medium"
+							<span class="font-roboto font-medium tracking-robotics text-secondary"
 								>University of Washington - Seattle</span
 							>
 						</p>
@@ -91,7 +91,7 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 			</div>
 		</section>
 		<section>
-			<RoboticsContainer class="md:max-w-4/5 xl:max-w-2/3 space-y-8 py-20">
+			<RoboticsContainer class="space-y-8 py-20 md:max-w-4/5 xl:max-w-2/3">
 				<p class="border-b-1 border-b-accent pb-5 text-lg">
 					The University of Washington’s Husky Robotics leads the frontier of collegiate planetary
 					exploration, blending elite engineering and scientific rigor. Our 2025 rover, Prometheus,
@@ -110,7 +110,10 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 					</div>
 					<div class="invisible xl:col-span-2">
 						<p class="font-roboto tracking-widest text-neutral-500">URC / CIRC COMPETITIONS</p>
-						<p class="text-lg font-bold text-black">May 27th to 30th <br /> August 7th to 10th</p>
+						<p class="text-lg font-bold text-black">
+							May 27th to 30th <br />
+							August 7th to 10th
+						</p>
 					</div>
 					<div class="invisible xl:col-span-2">
 						<p class="font-roboto tracking-widest text-neutral-500">OBJECTIVE</p>
@@ -122,21 +125,22 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 				</div>
 			</RoboticsContainer>
 		</section>
-		<section class="bg-primary md:py-30 py-20">
+		<section class="bg-primary py-20 md:py-30">
 			<RoboticsContainer class="flex flex-col items-center gap-10 lg:flex-row">
 				<div class="flex h-full flex-1 flex-col space-y-8">
 					<header class="space-y-2">
-						<p class="font-roboto tracking-widest text-zinc-400">University of Washington 2025</p>
-						<h2 class="text-secondary text-4xl font-extrabold">
+						<p class="font-roboto tracking-widest text-zinc-400">University of Washington 2026</p>
+						<h2 class="text-4xl font-extrabold text-secondary">
 							University Rover Challenge (URC) Team Video
 						</h2>
 					</header>
 					<p class="text-secondary">
-						Husky Robotics proudly earned selection among 38 global teams for URC 2025 against 100+
-						applicants, securing an exceptional SAR video score of 91.21. Continuing our legacy from
-						recent podium finishes, including multiple 2nd-place global wins at the Canadian
-						International Rover Challenge (CIRC). This video showcases Prometheus's autonomous
-						capabilities and our team's distinguished engineering excellence.
+						Husky Robotics has just submitted our 2026 System Acceptance Review presenting the
+						capabilities of our rover Daedalus and drone Icarus. This video showcases not only our
+						rovers abilities but is also a testament to the hard work our team has put into building
+						our rover this year. We aim to continue our legacy of podium finishes on the global
+						stage at the Canadian International Rover Challenge (CIRC) and the University Rover
+						Challenge (URC).
 					</p>
 					<ArrowLink to="#">Support Us</ArrowLink>
 				</div>
@@ -146,9 +150,17 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 							class="h-full w-full overflow-hidden rounded-sm"
 							width="560"
 							height="315"
-							src="https://www.youtube.com/embed/HaLj1IdKzLA?si=2HrCr6CkfrNutXKF"
+							src="https://www.youtube.com/embed/PJVO7Ae7_4M?si=CNEmDFMKfRhhFo_R"
 							title="YouTube video player"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allow="
+								accelerometer;
+								autoplay;
+								clipboard-write;
+								encrypted-media;
+								gyroscope;
+								picture-in-picture;
+								web-share;
+							"
 							referrerpolicy="strict-origin-when-cross-origin"
 							allowfullscreen
 						></iframe>
@@ -161,12 +173,12 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 			<RoboticsContainer class="space-y-20">
 				<header class="space-y-5 text-center">
 					<p class="font-roboto tracking-robotics text-zinc-400">Husky Robotics</p>
-					<h2 class="text-secondary text-5xl font-black">Meet Our Team</h2>
+					<h2 class="text-5xl font-black text-secondary">Meet Our Team</h2>
 					<p class="text-zinc-100">2025 Roster: <em>167 UW Engineers</em></p>
 				</header>
 				<div class="flex flex-col space-y-10">
 					<div
-						class="text-secondary flex flex-col items-center gap-5 lg:flex-row"
+						class="flex flex-col items-center gap-5 text-secondary lg:flex-row"
 						v-for="team in teams"
 						:key="team.name"
 					>
@@ -186,7 +198,7 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 								format="webp"
 								:alt="`Picture of ${team.name}.`"
 								:src="`/images/teams/${team.img}`"
-								class="aspect-3/2 lg:aspect-16/9 object-cover lg:p-5"
+								class="aspect-3/2 object-cover lg:aspect-16/9 lg:p-5"
 							/>
 						</div>
 					</div>
@@ -195,7 +207,7 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 					format="webp"
 					alt="Entire Robotics Team Photo"
 					src="/images/teams/full.jpg"
-					class="lg:max-w-3/5 mx-auto"
+					class="mx-auto lg:max-w-3/5"
 				/>
 				<ArrowLink
 					class="justify-center"
@@ -207,7 +219,7 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 		<section>
 			<RoboticsContainer class="flex flex-col py-10">
 				<div
-					class="border-t-1 flex flex-col gap-4 border-t-black py-8 lg:flex-row lg:items-center"
+					class="flex flex-col gap-4 border-t-1 border-t-black py-8 lg:flex-row lg:items-center"
 					v-for="category in gallery"
 					:key="category.name"
 				>
@@ -229,10 +241,10 @@ const { data: gallery } = await useAsyncData("gallery", () => queryCollection("g
 						<NuxtLink
 							aria-label="Learn More"
 							:to="{ path: '/gallery', query: { section: category.name.split(' ')[0] } }"
-							class="hover:text-primary flex items-center space-x-4 text-xl font-bold text-zinc-700 transition-colors duration-200 ease-in-out"
+							class="flex items-center space-x-4 text-xl font-bold text-zinc-700 transition-colors duration-200 ease-in-out hover:text-primary"
 							><p class="lg:hidden">Learn More</p>
 							<span
-								class="bg-tertiary lg:hover:bg-tertiary/75 flex h-6 w-6 items-center justify-center rounded-full transition-colors duration-200 ease-in-out lg:h-8 lg:w-8"
+								class="flex h-6 w-6 items-center justify-center rounded-full bg-tertiary transition-colors duration-200 ease-in-out lg:h-8 lg:w-8 lg:hover:bg-tertiary/75"
 								><i class="fa-solid fa-arrow-right fa-xs text-white"></i></span
 						></NuxtLink>
 					</div>
