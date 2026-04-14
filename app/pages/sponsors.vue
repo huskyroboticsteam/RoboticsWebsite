0195c00e855afaaf568b5a207b2ce83e999f528f
Jordan Tucker <jordan@jortuck.com>
2026-04-14 11:52:54 -0700

    update sponsors

--- app/pages/sponsors.vue
+++ app/pages/sponsors.vue
@@ -21,27 +21,38 @@ onMounted(() => {
 	});
 });
 const sponsors: Sponsor[] = [
-	{ name: "Mituoyo", logo: "mituoyo.png", url: "https://www.mitutoyo.com/" },
-	{ name: "Harmonic Drive", logo: "hd.gif", url: "https://www.harmonicdrive.net/" },
-	{ name: "Cubemars", logo: "cubemars.png", url: "https://www.cubemars.com/" },
-	{ name: "Husky Union Building", logo: "hub.png", url: "https://hub.washington.edu/" },
-	{ name: "Texas Instruments", logo: "ti.png", url: "https://www.ti.com/" },
+	// MPS
+	{ name: "Monolithic Power Systems", logo: "mps.png", url: "https://www.monolithicpower.com/" },
+
+	{ name: "Altium", logo: "altium.png", url: "https://www.altium.com/" },
+	{ name: "Solidworks", logo: "solidworks.png", url: "https://www.solidworks.com/" },
 	{ name: "OnShape", logo: "onshape.png", url: "https://www.onshape.com/" },
 	{
-		name: "University of Washington Department of Mechanical Engineering ",
-		logo: "me.gif",
+		name: "University of Washington Department of Mechanical Engineering",
+		logo: "me.png",
 		url: "https://me.washington.edu/"
+	},
+	{ name: "ODrive", logo: "odrive.png", url: "https://odriverobotics.com/" },
+
+	{
+		name: "University of Washington Department of Electrical Computer Engineering",
+		logo: "ece.png",
+		url: "https://ece.uw.edu/"
 	},
 	{
 		name: "Paul G. Allen School of Computer Science & Engineering",
 		logo: "cs.png",
 		url: "https://cs.washington.edu/"
 	},
-	{ name: "Altium", logo: "altium.png", url: "https://www.altium.com/" },
-	{ name: "Protocase", logo: "protocase.png", url: "https://www.protocase.com/" },
-	{ name: "Solidworks", logo: "solidworks.png", url: "https://www.solidworks.com/" },
-	{ name: "GMA Garnet", logo: "gma.png", url: "https://gmagarnet.com/en/americas" },
-	{ name: "Stoi Family", logo: "stoi.png", url: "https://stoiberengineering.com/" }
+	{ name: "Notion", logo: "notion.png", url: "https://www.notion.com/" },
+
+	{ name: "Allen Control Systems", logo: "acs.png", url: "https://www.allencontrolsystems.com/" },
+	{ name: "Hakko", logo: "hakko.png", url: "https://www.hakkousa.com/" },
+	{ name: "REV Robotics", logo: "rev.png", url: "https://www.revrobotics.com/" },
+	{ name: "Harmonic Drive", logo: "hd.gif", url: "https://www.harmonicdrive.net/" },
+	{ name: "Husky Union Building", logo: "hub.png", url: "https://hub.washington.edu/" },
+	{ name: "Texas Instruments", logo: "ti.png", url: "https://www.ti.com/" },
+	{ name: "Protocase", logo: "protocase.png", url: "https://www.protocase.com/" }
 ];
 </script>
 <template>
@@ -68,13 +79,15 @@ const sponsors: Sponsor[] = [
 						</div>
 					</div>
 					<div class="mx-auto mt-16 flex flex-col items-center space-y-6 px-4 text-center">
-						<p class="text-3xl font-extrabold tracking-wider">Thank You To Our 24-25 Sponsors</p>
+						<p class="text-3xl font-extrabold tracking-wider">
+							Thank You To Our 2025-2026 Sponsors
+						</p>
 						<p class="text-lg lg:max-w-1/2">
 							Thank you to our sponsors for empowering the Prometheus Rover’s mission. Your support
 							helps us design advanced systems, pursue ambitious goals, and push the boundaries of
 							student innovation in planetary exploration.
 						</p>
-						<p class="font-roboto tracking-robotics text-neutral-500">Husky Robotics Team (2025)</p>
+						<p class="font-roboto tracking-robotics text-neutral-500">Husky Robotics Team (2026)</p>
 					</div>
 				</div>
 			</RoboticsContainer>
