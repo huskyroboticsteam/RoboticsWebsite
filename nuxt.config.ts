bc970494dd917e066538a29c7556373e859e5ebc
Jordan Tucker <jordan@jortuck.com>
2026-04-28 10:28:14 -0700

    update prerender settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-				compatibilityDate: "2024-11-01",
-				devtools: { enabled: false },
-				modules: ["@vueuse/nuxt", "@nuxt/content", "@nuxt/image"],
-				vite: {
-								plugins: [tailwindcss()],
-								optimizeDeps: {
-												include: ["gsap", "gsap/ScrollTrigger", "gsap/SplitText", "gsap/ScrollToPlugin", "zod"]
-								}
-				},
-				css: ["~/assets/css/main.css"],
-				app: {
-								head: {
-												link: [{ rel: "icon", href: "/favicon.ico", type: "image/ico" }],
-												script: [{ src: "https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit" }]
-								}
-				},
-				ssr: true,
-				nitro: {
-								prerender: {
-												crawlLinks: true,
-												failOnError: false
-								}
-				},
-				runtimeConfig: {
-								public: {
-												turnstileKey: ""
-								},
-								turnstileSecret: "",
-								discordWebhook: "",
-								emailEndPoint: "",
-								emailSecret: ""
-				},
-				routeRules: {
-								"/": { prerender: true }
-				}
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: false },
+	modules: ["@vueuse/nuxt", "@nuxt/content", "@nuxt/image"],
+	vite: {
+		plugins: [tailwindcss()],
+		optimizeDeps: {
+			include: ["gsap", "gsap/ScrollTrigger", "gsap/SplitText", "gsap/ScrollToPlugin", "zod"]
+		}
+	},
+	css: ["~/assets/css/main.css"],
+	app: {
+		head: {
+			link: [{ rel: "icon", href: "/favicon.ico", type: "image/ico" }],
+			script: [{ src: "https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit" }]
+		}
+	},
+	ssr: true,
+	nitro: {
+		prerender: {
+			crawlLinks: true,
+			failOnError: false
+		}
+	},
+	runtimeConfig: {
+		public: {
+			turnstileKey: ""
+		},
+		turnstileSecret: "",
+		discordWebhook: "",
+		emailEndPoint: "",
+		emailSecret: ""
+	},
+	routeRules: {
+		"/**": { prerender: true },
+		"/api/**": { prerender: false }
+	}
 });
